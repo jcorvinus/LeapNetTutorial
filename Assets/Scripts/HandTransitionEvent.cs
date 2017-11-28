95c46285b8272023d9f36b9c26bd994e7e7d1c6d
Joshua Corvinus <dude.dudeness@gmail.com>
2017-11-28 16:22:24 -0500

    Initializing repo.

---
+++ Assets/Scripts/HandTransitionEvent.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using System.Collections;
+using Leap.Unity;
+
+public class HandTransitionEvent : HandTransitionBehavior
+{
+    public UnityEvent OnHandReset;
+    public UnityEvent OnHandFinish;
+
+    protected override void Awake()
+    {
+        base.Awake();
+    }
+
+    protected override void HandReset()
+    {
+        OnHandReset.Invoke();
+    }
+
+    protected override void HandFinish()
+    {
+        OnHandFinish.Invoke();
+    }
+}
---
+++ Assets/Scripts/NetHandVisibility.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+using System.Collections;
+
+using Leap.Unity;
+
+public class NetHandVisibility : NetworkBehaviour
+{
+    [SerializeField]
+    GameObject leftHand;
+    [SerializeField]
+    GameObject rightHand;
+
+    public void SetVisible(bool left)
+    {
+        CmdSetVisible(left, true);
+    }
+
+    public void SetInvisible(bool left)
+    {
+        CmdSetVisible(left, false);
+    }
+
+    [Command]
+    void CmdSetVisible(bool left, bool visible)
+    {
+        if (!isServer) return;
+
+        RpcSetVisible(left, visible);
+        DoActalVisibility(left, visible); // doing this so that our server updates?
+    }
+
+    [ClientRpc]
+    void RpcSetVisible(bool left, bool visible)
+    {
+        DoActalVisibility(left, visible);
+    }
+
+    private void DoActalVisibility(bool left, bool Visible)
+    {
+        GameObject handObject = (left) ? leftHand : rightHand;
+        handObject.SetActive(Visible);
+
+        Debug.Log(((left) ? "left" : "right") + ((Visible) ? "visible" : "invisible"));
+    }
+}
